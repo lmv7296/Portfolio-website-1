a85175b9b66e79df1bd480a8eb977b20d26388af
lmv7296 <74359101+lmv7296@users.noreply.github.com>
2023-07-14 12:44:59 -0300

    changed folder name and added new pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { FutureComponent } from './future/future.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    ProjectsComponent,
+    FutureComponent
   ],
   imports: [
     BrowserModule,
